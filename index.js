b5436a11b270ddf3656f25646fb010ad8dae3bb7
Armon Luckenbach <armonluck@gmail.com>
2023-08-26 18:07:43 -0400

    Commenting out socket.io code to make sure that app.listen is working properly

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { CORS_ORIGIN, BACKEND_URL, PORT } = process.env;
-const io = require('socket.io')(PORT)
+// const io = require('socket.io')(PORT)
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-// Socket.io
-io.on('connection', socket => {
-    console.log(socket.id);
-})
+// // Socket.io
+// io.on('connection', socket => {
+//     console.log(socket.id);
+// })
 
 app.listen(PORT, () => {
     console.log(`App listening at ${BACKEND_URL}:${PORT}`);
 });
 
-app.get("/", (req,res) => {    
+app.get("/", (req,res) => {  
     return res.send("Test successful");
 })
